8bf6feed5828655cd4c2ce2e261777748ea91eaf
Josh <diab0022@algonquinlive.com>
2019-04-28 01:14:42 -0400

    Minimal Reddit Client App Start

--- app/src/main/java/android/minimalredditclient/MainActivity.kt
+++ app/src/main/java/android/minimalredditclient/MainActivity.kt
@@ -2,11 +2,22 @@ package android.minimalredditclient
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
+import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolBar = findViewById<Toolbar>(R.id.reddit_toolbar)
+        setSupportActionBar(toolBar)
+    }
+
+    override fun onCreateOptionsMenu (m: Menu) : Boolean {
+        val inflater = menuInflater
+
+        inflater.inflate(R.menu.reddit_toolbar, m)
+        return true
     }
 }
